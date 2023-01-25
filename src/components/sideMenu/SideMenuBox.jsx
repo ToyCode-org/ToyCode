0977edebc3fe2a24369ba156f10946a7cd6c6724
LEE-YO-HAN <yhl0078@gmail.com>
2023-01-26 00:37:11 +0900

    Feat: SideMenu done

--- src/components/sideMenu/SideMenuBox.jsx
+++ src/components/sideMenu/SideMenuBox.jsx
@@ -1,39 +1,5 @@
 import styled from "styled-components";
 
-export const SideMenuBox = ({ menuOpen, setMenuOpen }) => {
-  return (
-    <>
-      {menuOpen ? (
-        <Background onClick={() => setMenuOpen(false)}>
-          <Container onClick={(e) => e.stopPropagation()}>
-            <MenuBox>메뉴 박스 내부</MenuBox>
-          </Container>
-        </Background>
-      ) : null}
-    </>
-  );
+export const SideMenuBox = () => {
+  return <>메뉴 박스 내부</>;
 };
-
-const Background = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.4);
-  z-index: 999;
-  cursor: auto;
-`;
-
-const Container = styled.div`
-  position: fixed;
-  left: 100%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const MenuBox = styled.div`
-  width: 50vw;
-  height: 100vh;
-  background-color: white;
-`;
--- src/pages/SideMenu.jsx
+++ src/pages/SideMenu.jsx
@@ -1,17 +1,63 @@
 import styled from "styled-components";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { SideMenuBox } from "../components/sideMenu/SideMenuBox";
 
 export const SideMenu = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   console.log(menuOpen);
 
+  const closeMenuHandler = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <>
-      <button onClick={() => setMenuOpen(menuOpen ? false : true)}>
-        사이드메뉴바입니다. <span>on</span> / <span>off</span>
-      </button>
-      <SideMenuBox menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      {!menuOpen ? (
+        <button onClick={() => setMenuOpen(true)}>사이드메뉴</button>
+      ) : null}
+      <Background
+        onClick={closeMenuHandler}
+        style={menuOpen ? { display: "block" } : { display: "none" }}
+      />
+      <Container
+        onClick={(e) => e.stopPropagation()}
+        style={
+          menuOpen
+            ? { transform: "translate(-50%, -50%)" }
+            : { transform: "translate(50%, -50%)" }
+        }
+      >
+        <MenuBox>
+          <SideMenuBox />
+          <button onClick={closeMenuHandler}>닫기버튼</button>
+        </MenuBox>
+      </Container>
     </>
   );
 };
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 0;
+  cursor: auto;
+`;
+
+const Container = styled.div`
+  position: fixed;
+  left: 85%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  transition: 0.3s;
+  z-index: 999;
+`;
+
+const MenuBox = styled.div`
+  width: 30vw;
+  height: 100vh;
+  background-color: white;
+`;
